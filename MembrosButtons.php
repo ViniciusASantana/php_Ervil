991615d5c03008bbb25ead7a0682c92d2a0cf98e
ViniciusASantana <61293575+ViniciusASantana@users.noreply.github.com>
2021-02-02 16:17:45 -0300

    Reporte

--- MembrosButtons.php
+++ MembrosButtons.php
@@ -11,10 +11,10 @@ $_SESSION['Membros_button']= $_POST['Membros_button'];
 
 
 print_r($_SESSION['ButtonUserID']);
-print_r($_SESSION['Membros_button']);
 print_r($_SESSION['comID']);
+
 switch ($_SESSION['Membros_button']){
-    case 0: break;
+    case 0: header('location:Reporte.php');break;
     case 1: {
         executarUpdate($con, "UPDATE Usuario_has_Comunidade SET cargo=cargo+1 WHERE usuario_idUsuario={$_SESSION['ButtonUserID']} AND comunidade_idComunidade={$_SESSION['comID']} AND cargo+1<3 OR usuario_idUsuario={$_SESSION['ButtonUserID']} AND comunidade_idComunidade={$_SESSION['comID']} AND cargo<{$_POST['suari']}"); 
         break;
@@ -28,5 +28,4 @@ switch ($_SESSION['Membros_button']){
         break;
     }
 }
-header('location:Membros.php');
 ?>
--- Reporte.php
+++ Reporte.php
@@ -1,37 +1,53 @@
 <?php
-$titulo = "Reporte";
-$alvo = "Bob Esponja Calça Quadrada";
-// um exemplo de trabalhar com arquivos externos 
+if(!isset($_SESSION)){
+    session_start();
+}
+$titulo = "Criar Postagem";
+$imagem= "imagens/usuario.png";
 include 'fragmentos/cabecalho.php';
 include 'fragmentos/menuResponsivo.php';
 
-?>
-<main class="container mt-5">
-    <div class="corpo2 m-auto">
-        <h2><strong>Reporte</strong></h2>
-        <br>
-        <div class="menu p-4 rounded-sm" >
-            <div class="form-group col">
-                Alvo:
-                <div class="conteiner-sm form-group p-2 border">
-                    <?= $alvo ?>
-               </div> 
+$_SESSION['ButtonUserID'];
 
-                Motivo:
-                <br>
-                <form action="#" method="GET">
-                    <textarea name="comentario" id="comentario" rows=3 cols=35 maxlength=80 placeholder=" Maxímo de 80 caracteres""></textarea>
-                </form>
+
+if(isset($_POST['motivo']) && isset($_SESSION['ButtonUserID'])){
+    if($_SESSION['login']==true)
+    executarInsert($con, "INSERT INTO ReporteUsuario(usuario,motivo) VALUES ({$_SESSION['ButtonUserID']},'{$_POST['motivo']}')");
+}
+
+$usuario= executarSelect($con, "SELECT * FROM usuario WHERE idUsuario={$_SESSION['ButtonUserID']}");
+?>
+
+<main class="container mt-5">
+    <div class=" mx-auto" style="width: 45vw;">
+        <h5 class="text-monospace"><strong>Reportar</strong></h5>
+        <hr>
+        <div class="conteiner form-group p-2 border" style="width: 15vw;">
+            <img src=<?= $imagem?> alt='Pedidos' class='rounded-circle' width='20' height='20'/>
+            <?= $usuario[0]['apelido']?>
+        </div>
+        
+            
+        <div class="p-4 d-flex align-content-center" style="background-color: whitesmoke;">
+            <form action="Reporte.php" method="POST">
                 
-            </div>
-        </div>    
-        <br>
-        <form action="#" method="GET">
-            <button type="submit" class="btn btn-info m-auto" id="disney">Enviar</button>
-        </form>
+                
+                <textarea name="motivo" id="criar"  maxlength=120 placeholder=" Motivo">
+
+                </textarea>
+                <br><br>
+                <button type='submit' class='btn bg-primary btn-outline-light' name="confirmar" value="1" id="confirmar"><strong>Enviar</strong></button>
+            </form>
+
+                
+        </div>
+        
+        
+        
     </div>
         
 </main>
+
 <?php
     include 'fragmentos/rodape.php';
 ?>
