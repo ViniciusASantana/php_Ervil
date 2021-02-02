7fc985efe56fabcf721b78df8a236b04dbf20ba3
ViniciusASantana <61293575+ViniciusASantana@users.noreply.github.com>
2021-02-02 16:26:15 -0300

    ReporteAtt

--- MembrosButtons.php
+++ MembrosButtons.php
@@ -17,15 +17,15 @@ switch ($_SESSION['Membros_button']){
     case 0: header('location:Reporte.php');break;
     case 1: {
         executarUpdate($con, "UPDATE Usuario_has_Comunidade SET cargo=cargo+1 WHERE usuario_idUsuario={$_SESSION['ButtonUserID']} AND comunidade_idComunidade={$_SESSION['comID']} AND cargo+1<3 OR usuario_idUsuario={$_SESSION['ButtonUserID']} AND comunidade_idComunidade={$_SESSION['comID']} AND cargo<{$_POST['suari']}"); 
-        break;
+        header('location:Membros.php');break;
     }
     case 2: {
         mysqli_query($con, "DELETE FROM Usuario_has_Comunidade WHERE usuario_idUsuario={$_SESSION['ButtonUserID']} AND comunidade_idComunidade={$_SESSION['comID']} AND cargo<3");
-        break;
+        header('location:Membros.php');break;
         }
     case 3: {
         executarUpdate($con, "UPDATE Usuario_has_Comunidade SET cargo=cargo-1 WHERE usuario_idUsuario={$_SESSION['ButtonUserID']} AND comunidade_idComunidade={$_SESSION['comID']} AND cargo-1>0"); 
-        break;
+        header('location:Membros.php');break;
     }
 }
 ?>
